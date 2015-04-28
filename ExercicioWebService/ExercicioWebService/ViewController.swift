dca24c94d45a6c4a0cfa2a523e65237a5b318b8e
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2015-04-28 08:59:24 -0300

    Alerta para inserir usuario

--- ExercicioWebService/ExercicioWebService/ViewController.swift
+++ ExercicioWebService/ExercicioWebService/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var MyTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func viewDidAppear(animated: Bool) {
+        self.addAlert()
+        
+            }
+    
+    
+    
+ func addAlert()
+ {
+    let alert:UIAlertController = UIAlertController(title: "Pesquisa de usuário", message: "Por Favor, insira o user desejado", preferredStyle: .Alert)
+    
+    alert.addTextFieldWithConfigurationHandler{
+        textField -> Void in
+        self.MyTextField = textField
+    }
+    
+    let action1:UIAlertAction = UIAlertAction(title: "OK", style: .Default) {action -> Void in
+        
+        if self.MyTextField.text == "" {
+            self.addAlert()
+        }
 
+        print(self.MyTextField.text)
+    }
+    
+    alert.addAction(action1)
+    
+    self.presentViewController(alert, animated: true, completion: {
+        
+    })
+ }
+    
+   
 }
 
